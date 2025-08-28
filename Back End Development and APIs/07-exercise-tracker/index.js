273c1d6c19312c7c3f9da104cc29ee085861a3e0
emma <123758288+Daffodil38@users.noreply.github.com>
2025-08-28 18:34:22 +0100

    finich exercise-tracker

--- Back End Development and APIs/07-exercise-tracker/index.js
+++ Back End Development and APIs/07-exercise-tracker/index.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -87,3 +98,8 @@ app.get('/api/users/:_id/logs', (req, res) => {
     log
   });
 });
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
